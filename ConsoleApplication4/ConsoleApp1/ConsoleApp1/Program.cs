6be665a4f0913bcf832e9b72b05d063d1993e156
martinnebesar <nebesar.martin@gmail.com>
2020-02-20 19:41:02 -0500

    Chapter4 uploaded

---
+++ ConsoleApplication4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        programstart:
+
+            Console.WriteLine("Enter an integer:");
+            int myInt = Convert.ToInt32(Console.ReadLine());
+
+            bool isLessThan10 = myInt < 10;
+
+            bool isBetween0And5 = (0 <= myInt) && (myInt <= 5);
+
+            if (isBetween0And5 == false)
+                goto programstart;
+
+            Console.WriteLine("Integer less than 10? {0}", isLessThan10);
+            Console.WriteLine("Integer between 0 and 5? {0}", isBetween0And5);
+
+            Console.WriteLine("Exactly one of the above is true? {0}", isLessThan10 ^ isBetween0And5);
+
+            string resultString = (isLessThan10 == true) ? "Less than 10" : "Greater than or equal to 10";
+
+            if (isLessThan10 == true)
+            {
+                // runs when exp is true
+                resultString = "Number is less than 10";
+            }
+            else
+            {
+                // runs when exp is false
+                resultString = "Number is greater than or equal to 10";
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comparison;
+            Console.WriteLine("Enter a number:");
+            double var1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter another number:");
+            double var2 = Convert.ToDouble(Console.ReadLine());
+            if (var1 < var2)
+                comparison = "less than";
+            else
+            {
+                if (var1 > var2)
+                    comparison = "greater than";
+                else
+                    comparison = "equal to";
+
+            }
+            Console.WriteLine("The first number is {0} the second number.", comparison);
+            Console.ReadKey();
+        }
+    }
+}
